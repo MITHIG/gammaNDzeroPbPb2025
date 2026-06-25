ce374c19264a1ba89a716200f401250e89fec37b
boundino <boundino@gmail.com>
2026-06-25 20:34:07 +0200

    add eff_calc

--- main/eff_calc.cc
+++ main/eff_calc.cc
@@ -1,5 +1,6 @@
 #include <TH3D.h>
 #include "xjjanauti.h"
+#include "xjjmypdf.h"
 
 #define __BINS_PTY_EQ__
 #include "../include/bins.h"
@@ -12,70 +13,102 @@ int macro(std::string inputname) {
   __XJJLOG << "++ info" << std::endl;
   xjjc::print_tab(info, -1);
 
-  auto ybin_analysis = xjjana::fixedbin_to_edges(bins::ny, bins::miny, bins::maxy);
+  auto ybin_analysis = xjjc::fixedbin_to_edges(bins::ny, bins::miny, bins::maxy);
+  __XJJLOG << "++ analysis binning" << std::endl;
+  xjjc::print_vec_h(ybin_analysis);
   
   std::map<std::string, TH3D*> h3s;
-  std::map<std::string, std::vector<TH1D*>> h1ys;
+  // std::map<std::string, std::vector<TH1D*>> h1ys;
   std::map<std::string, TH1D*> h1s;
-  std::map<std::string, TH1D*> h2s;
+  std::map<std::string, TH2D*> h2s;
 
   for (auto* h3 : xjjana::getobj_regexp<TH3D>(inf, "h3_.+")) {
     auto name = xjjc::str_eraseall(h3->GetName(), "h3_");
     h3->Sumw2();
     h3s[name] = h3;
     h3->GetZaxis()->SetRange(0, h3->GetZaxis()->GetNbins() + 1); // include overflow
-    h2s[name + "-y-pt"] = (TH2D*)h3->Project3D("xy");
-    h2s[name + "-y-pt"]->SetName(Form("h1_y-pt_%s", name.c_str()));
+    h2s[name + "-y-pt"] = (TH2D*)h3->Project3D("yx");
+    h2s[name + "-y-pt"]->SetName(Form("h2_y-pt_%s", name.c_str()));
     h1s[name + "-y"] = h3->ProjectionX(Form("h1_y_%s", name.c_str()),
                                        1, h3->GetYaxis()->GetNbins(),
                                        0, h3->GetZaxis()->GetNbins()+1, // overflow
                                        "e");
     h1s[name + "-y__rebin"] = (TH1D*)h1s[name + "-y"]->Rebin(ybin_analysis.size()-1, Form("%s__rebin", h1s[name + "-y"]->GetName()), ybin_analysis.data());
+    // h2s[name + "-y-pt__rebin"] = (TH2D*)h2s[name + "-y-pt"]->Rebin(ybin_analysis.size()-1, Form("%s__rebin", h2s[name + "-y-pt"]->GetName()), ybin_analysis.data());
+    // for (int i=0; i<h2s.at(name+"-y-pt__rebin")->GetXaxis()->GetNbins(); i++) {
+    //   auto* hpt = h2s.at(name + "-y-pt")->ProjectionY(Form("h1_pt_%s__rebin__y-%d", name.c_str(), i),
+    //                                                   i+1, i+1, "e");
+    //   h1ys[name + "-pt"].push_back(hpt);
+    // }
   }
   
   auto make_eff = [&h2s, &h1s](std::string name_new, std::string name_num, std::string name_den, std::string title) {
+    __XJJLOG << ">> " << name_new << std::endl;
     h2s[name_new + "-y-pt"] = (TH2D*)h2s.at(name_num + "-y-pt")->Clone(xjjc::str_replaceall(h2s.at(name_num + "-y-pt")->GetName(), name_num, name_new).c_str());
     h2s[name_new + "-y-pt"]->Divide(h2s.at(name_den + "-y-pt"));
     h2s[name_new + "-y-pt"]->GetZaxis()->SetTitle(title.c_str());
-    h1s[name_new + "-y"] = (TH2D*)h1s.at(name_num + "-y")->Clone(xjjc::str_replaceall(h1s.at(name_num + "-y")->GetName(), name_num, name_new).c_str());
+    h1s[name_new + "-y"] = (TH1D*)h1s.at(name_num + "-y")->Clone(xjjc::str_replaceall(h1s.at(name_num + "-y")->GetName(), name_num, name_new).c_str());
     h1s[name_new + "-y"]->Divide(h1s.at(name_den + "-y"));
     h1s[name_new + "-y"]->GetYaxis()->SetTitle(title.c_str());
-    h1s[name_new + "-y__rebin"] = (TH2D*)h1s.at(name_num + "-y__rebin")->Clone(xjjc::str_replaceall(h1s.at(name_num + "-y__rebin")->GetName(), name_num, name_new).c_str());
+    h1s[name_new + "-y__rebin"] = (TH1D*)h1s.at(name_num + "-y__rebin")->Clone(xjjc::str_replaceall(h1s.at(name_num + "-y__rebin")->GetName(), name_num, name_new).c_str());
     h1s[name_new + "-y__rebin"]->Divide(h1s.at(name_den + "-y__rebin"));
     h1s[name_new + "-y__rebin"]->GetYaxis()->SetTitle(title.c_str());
+    // for (int i=0; i<h1ys.at(name_num+"-pt").size(); i++) {
+    //   auto* heff_i = (TH1D*)h1ys.at(name_num+"-pt")[i]->Clone(xjjc::str_replaceall(h1ys.at(name_num+"-pt")[i]->GetName(), name_num, name_new).c_str());
+    //   heff_i->Divide(h1ys.at(name_den+"-pt")[i]);
+    //   heff_i->GetYaxis()->SetTitle(title.c_str());
+    //   h1ys[name_new + "-pt"].push_back(heff_i);
+    // }
   };
 
-  make_eff("eff", "eff_num", "eff_den", "#alpha #times #epsilon_{reco} #times #epsilon_{sel}");
-  make_eff("effreco", "reco_num", "eff_den", "#alpha #times #epsilon_{reco}");
-  make_eff("effsel", "eff_num", "reco_num", "#epsilon_{sel}");
+  make_eff("eff", "eff_num", "eff_den", xjjroot::CMS::D0 + "#scale[0.5]{ }#alpha#scale[0.5]{ }#times#scale[0.5]{ }#epsilon_{reco}#scale[0.5]{ }#times#scale[0.5]{ }#epsilon_{sel}");
+  make_eff("effreco", "reco_num", "eff_den", xjjroot::CMS::D0 + "#scale[0.5]{ }#alpha#scale[0.5]{ }#times#scale[0.5]{ }#epsilon_{reco}");
+  make_eff("effsel", "eff_num", "reco_num", xjjroot::CMS::D0 + "#scale[0.5]{ }#epsilon_{sel}");
+
+  xjjroot::print_tab(h2s, 0);
+  xjjroot::print_tab(h1s, 0);
+  // xjjroot::print_tab(h1ys, 0);
 
   if (h3s.find("data_signalwin") != h3s.end()) {
-    auto *heff = h2s.at("eff-y-pt"), *hdata = h2s.at("data_signalwin");
+    __XJJLOG << "++ data driven eff" << std::endl;
+  
+    auto *heff = h2s.at("eff-y-pt"), *hdata = h2s.at("data_signalwin-y-pt");
 
     auto make_effdata = [&h1s, &heff, &hdata](std::string name_refbin) {
       auto name_new = xjjc::str_replaceall(name_refbin, "eff", "effdata");
       h1s[name_new] = (TH1D*)h1s.at(name_refbin)->Clone(xjjc::str_replaceall(h1s.at(name_refbin)->GetName(), "eff", "effdata").c_str());
       h1s[name_new]->Reset();
-      std::vector<double> num_y(h1s.at(name_new)->GetXaxis()->GetNbins(), 0), den_y(num_y.size(), 0);
+      std::vector<double> sum_y(h1s.at(name_new)->GetXaxis()->GetNbins(), 0), norm_y(sum_y.size(), 0);
       for (int i=0; i<heff->GetXaxis()->GetNbins(); i++) {
         for (int j=0; j<heff->GetYaxis()->GetNbins(); j++) {
-          auto eff = heff->GetBinContent(i+1, j+1), // ! uncertainty
-            ndata = hdata->GetBinContent(i+1, j+1);
+          auto eff = heff->GetBinContent(i+1, j+1),
+            eff_e = heff->GetBinError(i+1, j+1),
+            ndata = hdata->GetBinContent(i+1, j+1),
+            ndata_e = hdata->GetBinError(i+1, j+1);
+            
           if (eff == 0) {
             __XJJLOG << "!! eff is 0 for the bin (" << i+1 <<", " << j+1 << ")" << std::endl;
             continue;
           }
-          ybin = h1s[name_new]->FindBin(heff->GetBinCenter(i+1));
-          num_y[ybin-1] += (1./eff)*ndata;
-          den_y[ybin-1] += ndata;
+          auto ybin = h1s.at(name_new)->FindBin(heff->GetXaxis()->GetBinCenter(i+1));
+          if (ybin < 1 || ybin > h1s.at(name_new)->GetXaxis()->GetNbins()) {
+            __XJJLOG << "?? bad FindBin (" << ybin << "). skip." << std::endl;
+            __XJJLOG << "   >> " << heff->GetXaxis()->GetBinCenter(i+1) << " -> " << ybin <<std::endl;
+            continue;
+          }
+          sum_y[ybin-1] += (1./eff)*ndata;
+          norm_y[ybin-1] += ndata;
         }
       }
+      
       for (int i=0; i<h1s.at(name_new)->GetXaxis()->GetNbins(); i++) {
-        if (num_y[i] == 0) {
-          __XJJLOG << "?? ndata is 0 for the bin (" << i+1 << ")" << std::endl;
+        if (sum_y[i] == 0) {
+          __XJJLOG << "?? ndata is 0 for the bin (" << i+1 << "). skip." << std::endl;
           continue;
         }
-        h1s.at(name_new)->SetBinContent(i+1, 1./(num_y[i]/den_y[i]));
+        
+        h1s.at(name_new)->SetBinContent(i+1, 1./(sum_y[i]/norm_y[i]));
+        h1s.at(name_new)->SetBinError(i+1, 0.001); // ! how to get the uncertainty ?
       }
     };
 
@@ -83,40 +116,79 @@ int macro(std::string inputname) {
     make_effdata("eff-y__rebin");
   }
 
-  xjjroot::print_tab(h2s, 0);
-  xjjroot::print_tab(h1s, 0);
-  for (auto& h : h2s)
-    xjjroot::sethempty(h.second, 0, 0);
-  for (auto& h : h1s) {
-    int color = xjjc::str_contains(h.first, "data") ? xjjroot::mycolor_satmiddle["red"] : kBlack;
-    xjjroot::setthgrstyle(h.second, color, 21, 1.2, color, 1, 1);
-    xjjroot::sethempty(h.second, 0, 0);
-    // xjjana::sethminmax(h.second, 0, 1.5);
+  for (auto& [_, h] : h2s) {
+    xjjroot::sethempty(h, 0, -0.1);
+    h->GetZaxis()->SetTitleSize(0);
+  }
+  for (auto& [name, h] : h1s) {
+    auto is_data = xjjc::str_contains(name, "data");
+    int color = is_data ? xjjroot::mycolor_satmiddle["red"] : kBlack;
+    xjjroot::setthgrstyle(h, color, is_data ? 20 : 21, 1.3, color, 1, 1);
+    xjjroot::sethempty(h, 0, 0.3);
+  }
+  // for (auto& [_, hh] : h1ys) {
+  //   for (int i=0; i<hh.size(); i++) {
+  //     xjjroot::sethempty(hh[i], 0, 0.3);
+  //     xjjroot::setthgrstyle(hh[i], xjjroot::colorlist_middle[i], 21, 1.3, xjjroot::colorlist_middle[i], 1, 1);
+  //   }
+  // }
+
+  auto draw_global = [&h2s, &info]() {
+    xjjroot::drawCMS(xjjroot::CMS::internal, info.at("inputmc_tex"));
+    xjjroot::drawtexgroup(1-gStyle->GetPadRightMargin()-0.06, 0.85, {
+        xjjc::number_range_string(h2s.at("eff-y-pt")->GetYaxis()->GetXmin(), h2s.at("eff-y-pt")->GetYaxis()->GetXmax(), "p_{T}") + " GeV",
+      }, 0.038, 33);
+  };
+  
+  xjjroot::setgstyle(1, 2, xjjroot::Colz);
+  auto* pdf = new xjjroot::mypdf(xjjc::str_replaceall(inputname, { { "saveeff", "calceff" }, { "rootfiles/", "figspdf/" }, { ".root", ".pdf" } }));
+
+  for (std::string name : { "eff", "effreco", "effsel" } ) {
+    pdf->prepare();
+    h2s[name + "-y-pt"]->Draw("colz");
+    xjjroot::drawtexgroup(0.18, 0.85, {
+        h2s.at(name + "-y-pt")->GetZaxis()->GetTitle()
+      }, 0.038, 13);
+    draw_global();
+    pdf->write();
   }
 
+  xjjroot::setcstyle(pdf->getc(), 1, xjjroot::Standard);
+  xjjroot::setgstyle(1, 2, xjjroot::Standard);
   
-  xjjroot::setgstyle(1);
-  auto* pdf = new xjjroot::mypdf(xjjc::str_replaceall(inputname, { { "saveeff", "calceff" }, { ".root", ".pdf" } }));
-
   pdf->prepare();
-  h2s["eff-y-pt"]->Draw("colz");
+  gPad->Modified();
+  gPad->Update();
+  xjjana::sethabsminmax(h1s["eff-y__rebin"], 0, .2);
+  h1s["eff-y__rebin"]->Draw("pe1");
+  h1s["effdata-y__rebin"]->Draw("pe1 same");
+  draw_global();
   pdf->write();
 
   pdf->prepare();
-  h1s["eff-y__rebin"]->Draw("pe");
-  h1s["effdata-y__rebin"]->Draw("pe same");
+  xjjana::sethabsminmax(h1s["effreco-y__rebin"], 0, 1.);
+  h1s["effreco-y__rebin"]->Draw("pe1");
+  draw_global();
   pdf->write();
 
   pdf->prepare();
-  h1s["effreco-y__rebin"]->Draw("pe");
-  pdf->write();
-
-  pdf->prepare();
-  h1s["effsel-y__rebin"]->Draw("pe");
+  xjjana::sethabsminmax(h1s["effsel-y__rebin"], 0, .2);
+  h1s["effsel-y__rebin"]->Draw("pe1");
+  draw_global();
   pdf->write();
 
   pdf->close();
 
+  auto* outf = xjjroot::newfile(xjjc::str_replaceall(inputname, "saveeff", "calceff"));
+  for (auto& [_, h] : h2s) xjjroot::writehist(h);
+  for (auto& [_, h] : h1s) xjjroot::writehist(h);
+  auto* t = new TTree("info", "");
+  for (auto& [key, content] : info) {
+    t->Branch(key.c_str(), &content);
+  }
+  t->Fill();
+  t->Write();
+  outf->Close();
   
   return 0;
 }
---
+++ main/run_main.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+INPUTS_DATA=(
+    "/eos/cms/store/group/phys_heavyions/wangj/Forest2025PbPb/Dzero_260426-yrefmva_PbPbUPC_HIForward_Dpt-2_Dsize_24PD.root;2025 PbPb (5.36 TeV);2025PbPb;"
+    # "/eos/cms/store/group/phys_heavyions/wangj/Forest2023PbPb/Dzero_260212-hfle_2023PbPbUPC_Jan2024ReReco_20260212Forest_HIForward_Dpt-2_Trig-2_Dsize_xbr.root;2023 PbPb (Jan2024 ReReco);2023PbPb-recoJan2024;"
+    # "/eos/cms/store/group/phys_heavyions/wangj/Forest2023PbPb/Dzero_260426-yrefmva_2023PbPbUPC_Feb2025ReReco_20260521Forest_HIForward_Dpt-2_Trig-2_Dsize.root;2023 PbPb (Feb2025 ReReco);2023PbPb-recoFeb2025;"
+)
+CUTEVTS=(
+    "ZDCgammaN && HFEMaxPlus_eta5 < 16 && selectedVtxFilter;#gammaN (25);gammaN-25" # HLT and cscTightHalo2015Filter is hard coded in .cc
+    # "ZDCgammaN && HFEMaxPlus_eta5 < 9.2 && ClusterCompatibilityFilter && selectedVtxFilter && nVtx <= 3;#gammaN (23);gammaN-23"
+)
+INPUT_MASS_TEMPLATE="/eos/cms/store/group/phys_heavyions/wangj/Forest2024PbPb/Dzero_260311-ydiffmva_HiForest_260218_prompt_GNucleusToD0-PhotonBeamA_Bin-Pthat0_Kpi_t2_Dpt-2_Dsize.root;P#scale[0.85]{YTHIA}8#scale[0.5]{ }#gammaN (5.36 TeV);"
+INPUTS_MC=(
+    "/eos/cms/store/group/phys_heavyions/wangj/Forest2024PbPb/Dzero_260426-yrefmva_HiForest_260328_prompt_GNucleusToD0-PhotonBeamA_Bin-Pthat0_Kpi_trkpt0p1_Drej-genmatched_Dpt-2.root;P#scale[0.85]{YTHIA}8#scale[0.5]{ }#gammaN (5.36 TeV);2024-Pthat0-BeamA"
+)
+CUT_BASE="Dpt>=2 && Dpt<5 && fabs(Dy)<2 && TMath::Abs(Dtrk1PtErr/Dtrk1Pt)<0.1 && TMath::Abs(Dtrk2PtErr/Dtrk2Pt)<0.1 && Dtrk1Pt > 0.5 && Dtrk2Pt > 0.5 && TMath::Abs(Dtrk1Eta) < 2.4 && TMath::Abs(Dtrk2Eta) < 2.4 && Dchi2cl > 0.05 && (DsvpvDistance/DsvpvDisErr) > 1. && DsvpvDisErr>1.e-8 && DsvpvDisErr_2D>1.e-8"
+CUTDS=(
+    "${CUT_BASE} && (Dtrk1PixelHit+Dtrk1StripHit)>=11 && (Dtrk2PixelHit+Dtrk2StripHit)>=11 && DpassCut23PAS;Cut23PAS;D23pas"
+)
+
+##
+
+make save_hist.exe fit_hist.exe eff_save.exe eff_calc.exe || exit 1
+
+for cutevtstr in "${CUTEVTS[@]}" ; do
+    IFS=';' ; cutevttags=($cutevtstr) ; unset IFS ; cutevt="${cutevttags[0]}" ; cutevt_tex="${cutevttags[1]}" ; cutevt_tag="${cutevttags[2]}"
+
+    [[ x$cutevt_tag == x ]] && { echo "warning: missed cutevt_tag. skip." ; continue ; }
+
+    itag_event=$cutevt_tag"/event_eff" #
+
+    ## Loop cuts
+    for cutdstr in "${CUTDS[@]}" ; do
+        IFS=';' ; cutdtags=($cutdstr) ; unset IFS ; cutd="${cutdtags[0]}" ; cutd_tex="${cutdtags[1]}" ; cutd_tag="${cutdtags[2]}"
+
+        [[ x$cutd_tag == x ]] && { echo "warning: missed cutd_tag. skip." ; continue ; }
+
+        cut_tag=$cutevt_tag"_"$cutd_tag
+        cutstr=${cutevt}" && "${cutd}" ; "${cutevt_tex}", "${cutd_tex}" ; "$cut_tag
+
+        echo -e "\033[33m"$cut_tag"\033[0m"
+        echo -e "\033[2m"$cutstr"\033[0m"
+
+        itag_template=$cut_tag"/mass_templates" #
+        [[ ${1:-0} -eq 1 ]] && {
+            # ./save_hist.exe "$INPUT_MASS_TEMPLATE" "$cutstr" $itag_template 0
+            ./save_hist.exe "$INPUT_MASS_TEMPLATE" "${CUT_BASE};D precut;Dpre" $itag_template 0
+        }
+
+        ## Loop data
+        for input_data in "${INPUTS_DATA[@]}" ; do
+            
+            IFS=';' ; input_data_tags=($input_data) ; unset IFS ; data_tag=${input_data_tags[2]} ; data_lumi=${input_data_tags[3]} ;
+            echo -e "  \033[33m"$data_tag"\033[0m: "$data_lumi" mb-1"
+
+            itag_data=$cut_tag"/savehist_"$data_tag
+            [[ ${2:-0} -eq 1 ]] && {
+                ./save_hist.exe "$input_data" "$cutstr" $itag_data 
+            }
+
+            itag_data_fit=$cut_tag"/fithist_"$data_tag
+            [[ ${3:-0} -eq 1 ]] && {
+                ./fit_hist.exe "rootfiles/"$itag_data".root" "rootfiles/"$itag_template".root"
+            }
+
+            ## Loop MC
+            for input_mc in "${INPUTS_MC[@]}" ; do
+
+                IFS=';' ; input_mc_tags=($input_mc) ; unset IFS ; mc_tag=${input_mc_tags[2]} ;
+                echo -e "    \033[33m"$mc_tag"\033[0m"
+
+                itag_deff=$cut_tag"/saveeff_"$mc_tag"_"$data_tag
+                [[ ${4:-0} -eq 1 ]] && {
+                    ./eff_save.exe "$input_mc" "$cutevtstr" "$cutdstr" $itag_deff "$input_data"
+                }
+                [[ ${5:-0} -eq 1 ]] && {
+                    ./eff_calc.exe "rootfiles/"$itag_deff".root"
+                }
+
+            done
+        done
+    done 
+done
+
